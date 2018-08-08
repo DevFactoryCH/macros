e33ade9272b01393afd61cf2af40d032ac0f0dc7
mark.cameron <mark.cameron@devfactory.ch>
2018-08-08 10:15:25 +0200

    Added missing ID for delete button confirmation modal

--- src/views/macros/item_button_delete.blade.php
+++ src/views/macros/item_button_delete.blade.php
@@ -1,5 +1,5 @@
 <div class="btn-group">
-  {{ Form::open(['method' => 'DELETE', 'url' => $url]) }}
+  {{ Form::open(['method' => 'DELETE', 'url' => $url, 'class' => 'form-delete-button', 'id' => 'delete-id-'. $id]) }}
   {{ Form::button($text, $attributes) }}
   {{ Form::close() }}
 </div>
